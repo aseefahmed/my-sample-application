90704cdd4f9322d68c9e44c32c7257f7c71cbd12
aseef ahmed <aseefahmed@gmail.com>
2020-06-14 23:03:33 +1200

    version two changes

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Welcome to our sample GO Application"}
+		res := &response{Message: "Welcome to our sample GO Application : Version 2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
